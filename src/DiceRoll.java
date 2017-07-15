43ad31b9c706059f07b47cbc69f356afc8bdf77a
David B Valentine II <davidbv2@gmail.com>
2017-07-15 18:13:20 -0400

    Made program continous, based on user input

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -5,29 +5,39 @@ import java.util.Random;
 import java.util.Scanner;
 public class DiceRoll {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         int zero = 0;
+        String answer = " ";
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Welcome to the Grand Circus Casino!");
-        System.out.println("Please enter the number of sides for a pair of dice: ");
-        int diceSideNum = scan.nextInt();
-        System.out.println("There are " + diceSideNum + " sides to each of your die.");
+        //start do while loop, while
 
-        if (rollDie(diceSideNum) == zero)
-        { System.out.println(0 + 1);
-        } else System.out.println(rollDie(diceSideNum));
 
-        if (rollDie(diceSideNum) == zero){
-            System.out.println(0 + 1);
-        } else System.out.println(rollDie(diceSideNum));
+            System.out.println("Welcome to the Grand Circus Casino!");
+
+            do {
+
+            System.out.println("Please enter the number of sides for a pair of dice: ");
+            int diceSideNum = scan.nextInt();
+            scan.nextLine();
+            System.out.println("There are " + diceSideNum + " sides to each of your die.");
+
+            System.out.println(rollDie(diceSideNum));
+            System.out.println(rollDie(diceSideNum));
+
+            System.out.println("Would you like to continue? Please enter 'yes' or 'no'.");
+            answer = scan.nextLine();
+
+            System.out.println();
+
+        } while (answer.equalsIgnoreCase("yes"));
+
     }
-
 
     public static int rollDie(int a) {
 
         Random randNum = new Random();
-        return randNum.nextInt(a);
+        return (randNum.nextInt(a)) + 1;
 
     }
 
